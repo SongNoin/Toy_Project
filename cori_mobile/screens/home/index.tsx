47e589be8cc7d45ed14d20f96c00637a6d5f20d1
SongNoin <songnoin@Cori-SongNoin-MacBookPro.local>
2022-05-24 16:58:12 +0900

    푸시테스트

--- cori_mobile/screens/home/index.tsx
+++ cori_mobile/screens/home/index.tsx
@@ -4,7 +4,7 @@ import {View, Text} from 'react-native';
 export default function HomeScreen() {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>홈스크린</Text>
+      <Text>홈스크린테스트</Text>
     </View>
   );
 }
